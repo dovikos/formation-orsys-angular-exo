491b8e00a5fb92e5a6b4dece2fd2886f36501bac
Ludovic Heitz <ludovic.heitz@cgi.com>
2017-04-11 16:06:38 +0200

    Add: cgi-route

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('main', ['cgi-list']);
+	var app = angular.module('main', ['cgi-list', 'cgi-route']);
+
+	app.config(function() {
+		console.log('config main');
+	});
+
+	app.run(function() {
+		console.log('run main');
+	});
 
 	app.directive('cgiHeader', function() {
 		return {
@@ -10,17 +18,17 @@
 		};
 	});
 
-    app.directive('cgiBody', function() {
-        return {
-            restrict: 'E',
-            templateUrl: './tmpl/cgi-body.html'
-        }
-    });
+	app.directive('cgiBody', function() {
+		return {
+			restrict: 'E',
+			templateUrl: './tmpl/cgi-body.html'
+		}
+	});
 
-    app.directive('cgiFooter', function() {
-        return {
-            restrict: 'E',
-            templateUrl: './tmpl/cgi-footer.html'
-        }
-    });
+	app.directive('cgiFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: './tmpl/cgi-footer.html'
+		}
+	});
 })();
---
+++ app/cgi-route/cgi-route.js
@@ -0,0 +1,42 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('cgi-route', ['ui.router']);
+
+	app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+		'ngInject';
+
+		$locationProvider
+			.html5Mode(true);
+
+		var homeState = {
+			name: 'home',
+			url: '/',
+			templateUrl: './cgi-route/tmpl/home.html'
+		}
+
+		var productState = {
+			name: 'produits',
+			url: '/produits',
+			templateUrl: './cgi-route/tmpl/produits.html'
+		}
+
+		var serviceState = {
+			name: 'services',
+			url: '/services',
+			templateUrl: './cgi-route/tmpl/services.html'
+		}
+
+		var contactState = {
+			name: 'contact',
+			url: '/contact',
+			templateUrl: './cgi-route/tmpl/contact.html'
+		}
+
+		$stateProvider.state(homeState);
+		$stateProvider.state(productState);
+		$stateProvider.state(serviceState);
+		$stateProvider.state(contactState);
+		$urlRouterProvider.otherwise('/');
+	});
+})();
