81a777306a7e38e02e2b152a91f74e1efff4813a
Ludovic Heitz <ludovic.heitz@cgi.com>
2017-04-11 12:15:46 +0200

    Finished cgi-list

--- app/cgi-list/cgi-list.js
+++ app/cgi-list/cgi-list.js
@@ -3,7 +3,8 @@
 
 	var app = angular.module('cgi-list', []);
 
-	app.run(['$rootScope', function($rootScope) {
+	app.run(function($rootScope) {
+		'ngInject';
 		$rootScope.affaires = [];
 
 		for (var i = 0; i < 10000; i++) {
@@ -12,13 +13,27 @@
 				description: 'Description ' + i
 			});
 		}
-	}]);
+		console.log('$rootScope.affaires', $rootScope.affaires);
+	});
 
-	app.directive('cgi-list', function() {
+	app.directive('cgiList', function() {
 		return {
 			restrict: 'E',
-			controller: function CgiListCtrl() {
+			controller: function CgiListCtrl($scope, $element, $attrs, $rootScope, $compile) {
+				'ngInject';
 				console.log('CgiListCtrl', arguments);
+				var content = $element.html();
+				$element.html('');
+
+				for (var i = 0; i < 10; i++) {
+					var html = '<div>' + content + '</div>';
+                    var elt = angular.element(html);
+					$element.append(elt);
+                    var scope = $scope.$new(false);
+                    var name = $attrs.name;
+                    scope[name] = $rootScope.affaires[i];
+					$compile(elt)(scope);
+				}
 			}
 		}
 	});
