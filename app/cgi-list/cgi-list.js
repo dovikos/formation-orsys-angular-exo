610c46138405bb16b13e1ec3716d878f64ab3a02
Ludovic Heitz <ludovic.heitz@cgi.com>
2017-04-12 10:41:46 +0200

    Update: création de deux services

--- app/cgi-list/cgi-list.js
+++ app/cgi-list/cgi-list.js
@@ -24,50 +24,77 @@
 		//console.log('$rootScope.affaires', $rootScope.affaires);
 	});
 
+	app.service('infiniteListSrv', function InfiniteListSrv($rootScope) {
+		'ngInject';
+		this.getMore = function(start, qty) {
+			if (isNaN(qty)) {
+				console.error('\'qty\' is not a number', qty);
+				return;
+			}
+
+			var result = $rootScope.affaires.slice(start, start + qty);
+			start += qty;
+			return result;
+		}
+	});
+
+	app.service('debounceSrv', function DebounceSrv() {
+		var map = {};
+		this.run = function(name, offset, callback) {
+			if (!map[name]) {
+				map[name] = {};
+			}
+			if (!map[name].lastCall) {
+				map[name].lastCall = new Date().getTime();
+				callback();
+				return;
+			}
+			if (map[name].lastCall > new Date().getTime() - offset) {
+				return;
+			}
+			map[name].lastCall = new Date().getTime();
+			callback();
+		};
+	});
+
 	app.directive('cgiList', function() {
 		return {
 			restrict: 'E',
-			controller: function CgiListCtrl($scope, $element, $attrs, $rootScope, $compile) {
+			controller: function CgiListCtrl($scope, $element, $attrs,
+				$rootScope, $compile, infiniteListSrv, debounceSrv) {
 				'ngInject';
 				var ctrl = this;
 				console.log('CgiListCtrl', arguments);
 				var content = $element.html();
 				$element.html('');
 
-				ctrl.start = 0;
-
 				window.onwheel = function() {
 					console.log('Scrollllle', arguments);
-					ctrl.getMore();
+					debounceSrv.run('cgiList', ctrl.offset, ctrl.getMore);
 					$scope.$apply();
 				};
 
 				ctrl.$onInit = function() {
-					ctrl.lastCall = new Date();
-					ctrl.offset = 0.5;
+					ctrl.offset = 5000;
+					ctrl.start = 0;
 				};
 
 				ctrl.getMore = function() {
-					ctrl.now = new Date();
-					var d = new Date(ctrl.lastCall);
-					d.setSeconds(d.getSeconds + ctrl.offset);
-					if (ctrl.now < d) {
-						return;
-					}
-					ctrl.lastCall = ctrl.now;
 					var qty = $attrs.qty || 10;
 					qty = Number(qty);
 
-					for (var i = ctrl.start; i < ctrl.start + qty; i++) {
+					var array = infiniteListSrv.getMore(ctrl.start, qty);
+					ctrl.start += qty;
+
+					for (var i = 0; i < qty; i++) {
 						var html = '<div>' + content + '</div>';
 						var elt = angular.element(html);
 						$element.append(elt);
 						var scope = $scope.$new(false);
 						var name = $attrs.name;
-						scope[name] = $rootScope.affaires[i];
+						scope[name] = array[i];
 						$compile(elt)(scope);
 					}
-					ctrl.start += qty;
 				}
 
 				ctrl.getMore();
